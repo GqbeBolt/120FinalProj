3516d8d2fb5160db334644899d6e1893c4f2d3a0
GqbeBolt <95443729+GqbeBolt@users.noreply.github.com>
2026-02-24 19:10:48 -0800

    blue prince progress

--- src/scenes/BluePrincePuzzle.js
+++ src/scenes/BluePrincePuzzle.js
@@ -3,6 +3,18 @@ class BluePrincePuzzle extends Phaser.Scene {
         super("blueScene");
     }
 
+    init() {
+        this.colorArr = ["black", "red", "violet", "pink", "green", "yellow", "orange", "white"];
+        this.travelArr = ["naval", "roads", "horseback", "trains", "aviation", "camels", "turtleback"];
+        this.weatherArr = ["normal", "foggy", "hot", "rainy", "stormy", "snow", "windy"];
+        this.societyArr = ["tribal", "agricultural", "martial", "metropolitan", "academic", "industrial", "spiritual", "poetic"];
+
+        this.currColor = "black";
+        this.currTravel = "naval";
+        this.currWeather = "normal";
+        this.currSociety = "tribal";
+    }
+
     create() {
         
     }
