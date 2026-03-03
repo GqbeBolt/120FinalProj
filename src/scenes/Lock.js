e84a6b6fd6ce33b0deaf301b8d08c3793f8ea959
GqbeBolt <95443729+GqbeBolt@users.noreply.github.com>
2026-03-02 18:49:48 -0800

    lock functionalty progress

--- src/scenes/Lock.js
+++ src/scenes/Lock.js
@@ -12,5 +12,16 @@ class Lock extends Phaser.Scene {
         this.add.image(0, 0, "noiseBG").setOrigin(0).setAlpha(0.5);
 
         this.add.image(width/2, height/2, "lockTemp").setOrigin(0.5);
+
+        this.oneUp = this.add.image(407, 199, "arrowUp").setOrigin(0).setInteractive({
+            hitArea: new Phaser.Geom.Rectangle(0, 14, 32, 18),
+            hitAreaCallback: Phaser.Geom.Rectangle.Contains,
+            useHandCursor: true
+        })
+        .on("pointerdown", () => {
+
+        })
+        .on("pointerover", () => {this.oneUp.setTint(blueHex)})
+        .on("pointerout", () => {this.oneUp.setTint(0xFFFFFF)});
     }
 }
